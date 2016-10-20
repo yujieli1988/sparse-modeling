4eeec04a5efe965968924db03edfccbcd70d437e
yujieli1988 <yujie-li@aist.go.jp>
2016-10-20 11:46:56 +0900

    Update run_smrs_Y.m

--- run_smrs_Y.m
+++ run_smrs_Y.m
@@ -1,11 +1,6 @@
 clear all;
-load Ymul.mat
-%Ynon=Ynon(:,1:11370);
 alpha = 2;
 r = 30;
 verbose = true;
 [ind, C] = smrs(Ymul, alpha, r, verbose);
-
-%%
-
- save ('ind.mat','ind');
+save ('ind.mat','ind');
